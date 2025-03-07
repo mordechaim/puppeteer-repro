566a5c2e9eb1fdac35b9ddb4c1b4c73195d0fb2e
Mordechai Meisels <mordechai@codaworks.com>
2025-03-07 10:53:04 -0500

    Reproduction

---
+++ app/browser.ts
@@ -0,0 +1,41 @@
+'use server';
+
+import chromium from '@sparticuz/chromium-min';
+import puppeteer from 'puppeteer-core';
+
+const launch = async () => {
+  return await puppeteer.launch({
+    pipe: true,
+    dumpio: true,
+    args: [
+      ...chromium.args,
+      '--disable-setuid-sandbox',
+      '--no-sandbox',
+      '--no-zygote',
+    ],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: process.env.CHROMIUM_LOCAL
+      ? puppeteer.executablePath('chrome')
+      : await chromium.executablePath(
+          'https://github.com/Sparticuz/chromium/releases/download/v132.0.0/chromium-v132.0.0-pack.tar'
+        ),
+  });
+};
+
+export const screenshot = async (html: string) => {
+  const browser = await launch();
+  const tab = await browser.newPage();
+  await tab.setContent(html, {
+    timeout: 0,
+    waitUntil: ['load'],
+  });
+
+  const bytes = await tab.screenshot({
+    type: 'png',
+    fullPage: true,
+    omitBackground: true,
+  });
+  await browser.close();
+
+  return Buffer.from(bytes).toString('base64');
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,51 @@
-/** Add your relevant code here for the issue to reproduce */
+'use client';
+
+import { ChangeEvent, useState, useTransition } from 'react';
+import { screenshot } from './browser';
+
 export default function Home() {
-  return null;
+  const [pending, startTransition] = useTransition();
+  const [data, setData] = useState<string>();
+
+  const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) {
+      return;
+    }
+
+    startTransition(async () => {
+      const { promise, resolve } = Promise.withResolvers<string>();
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        resolve(e.target?.result as string);
+      };
+
+      reader.readAsText(file);
+      const html = await promise;
+
+      setData(await screenshot(html));
+    });
+  };
+
+  return (
+    <div>
+      {!pending && !data && (
+        <label>
+          Create screenshot
+          <input
+            type='file'
+            style={{ display: 'none' }}
+            onChange={handleFile}
+            accept='text/html'
+          />
+        </label>
+      )}
+
+      {pending && <span>Generating...</span>}
+
+      {data && (
+        <img src={`data:image/png;base64,${data}`} style={{ width: '100vw' }} />
+      )}
+    </div>
+  );
 }
